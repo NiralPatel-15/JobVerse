b1d074cde07b1c35ff063be6e53c817c1bf09df3
Niral Patel <niralpatel1502@gmail.com>
2026-06-30 23:07:27 +0530

    Update socket.js

--- jobverse-frontend/src/socket/socket.js
+++ jobverse-frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL || "http://localhost:4000", {
+const socket = io(import.meta.env.VITE_API_URL || "https://jobverse-api.onrender.com", {
   withCredentials: true,
   transports: ["websocket"],
 });
