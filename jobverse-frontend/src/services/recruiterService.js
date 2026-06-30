56a47387a5843dae10267b31bc18887038078cad
Niral Patel <niralpatel1502@gmail.com>
2026-06-30 23:05:41 +0530

    Update recruiterService.js

--- jobverse-frontend/src/services/recruiterService.js
+++ jobverse-frontend/src/services/recruiterService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://jobverse-api.onrender.com/api",
   withCredentials: true, // IMPORTANT
 });
 
